4c1d574d6b78336b8a106470b80e18d8bb1b10a4
change27 <change27@gmail.com>
2021-06-06 22:59:25 +0900

    ECREDIT

--- src/main/java/lecture/AdvertisementController.java
+++ src/main/java/lecture/AdvertisementController.java
@@ -4,15 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Map;
+
 
  @RestController
  public class AdvertisementController {
